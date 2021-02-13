80fbe58a8b390d1dad3dfc5d22039a7aa8858381
Айрат Салахов <salakhovairatz@gmail.com>
2021-02-13 12:48:25 +0300

    first commit :)

---
+++ Race/Form_main.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Race
+{
+    partial class Form_main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_main));
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_settings = new System.Windows.Forms.Button();
+            this.button_exit = new System.Windows.Forms.Button();
+            this.button_help = new System.Windows.Forms.Button();
+            this.label_help = new System.Windows.Forms.Label();
+            this.button_ok = new System.Windows.Forms.Button();
+            this.checkBox_audio = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.Color.Salmon;
+            this.button_start.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_start.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_start.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_start.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_start.Location = new System.Drawing.Point(346, 170);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(100, 30);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start game";
+            this.button_start.UseVisualStyleBackColor = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_settings
+            // 
+            this.button_settings.BackColor = System.Drawing.Color.Salmon;
+            this.button_settings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_settings.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_settings.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_settings.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_settings.Location = new System.Drawing.Point(346, 206);
+            this.button_settings.Name = "button_settings";
+            this.button_settings.Size = new System.Drawing.Size(100, 30);
+            this.button_settings.TabIndex = 2;
+            this.button_settings.Text = "Settings";
+            this.button_settings.UseVisualStyleBackColor = false;
+            this.button_settings.Click += new System.EventHandler(this.button_settings_Click);
+            // 
+            // button_exit
+            // 
+            this.button_exit.BackColor = System.Drawing.Color.Salmon;
+            this.button_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_exit.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_exit.Location = new System.Drawing.Point(346, 278);
+            this.button_exit.Name = "button_exit";
+            this.button_exit.Size = new System.Drawing.Size(100, 30);
+            this.button_exit.TabIndex = 3;
+            this.button_exit.Text = "Exit game";
+            this.button_exit.UseVisualStyleBackColor = false;
+            this.button_exit.Click += new System.EventHandler(this.button_exit_Click);
+            // 
+            // button_help
+            // 
+            this.button_help.BackColor = System.Drawing.Color.Salmon;
+            this.button_help.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_help.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_help.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_help.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_help.Location = new System.Drawing.Point(346, 242);
+            this.button_help.Name = "button_help";
+            this.button_help.Size = new System.Drawing.Size(100, 30);
+            this.button_help.TabIndex = 4;
+            this.button_help.Text = "How to play";
+            this.button_help.UseVisualStyleBackColor = false;
+            this.button_help.Click += new System.EventHandler(this.button_help_Click);
+            // 
+            // label_help
+            // 
+            this.label_help.AutoSize = true;
+            this.label_help.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_help.Location = new System.Drawing.Point(122, 157);
+            this.label_help.Name = "label_help";
+            this.label_help.Size = new System.Drawing.Size(555, 115);
+            this.label_help.TabIndex = 5;
+            this.label_help.Text = "You have to avoid hitting an enemy car\r\nUse WASD to navigate your car\r\nTry to sco" +
+    "re as much as you can\r\nPress SPACE to put a game into pause\r\nYou can change the " +
+    "color or speed of your car in settings";
+            this.label_help.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label_help.Visible = false;
+            // 
+            // button_ok
+            // 
+            this.button_ok.BackColor = System.Drawing.Color.Salmon;
+            this.button_ok.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button_ok.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_ok.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button_ok.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ok.Location = new System.Drawing.Point(346, 278);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(100, 30);
+            this.button_ok.TabIndex = 6;
+            this.button_ok.Text = "Got it!";
+            this.button_ok.UseVisualStyleBackColor = false;
+            this.button_ok.Visible = false;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // checkBox_audio
+            // 
+            this.checkBox_audio.AutoSize = true;
+            this.checkBox_audio.BackColor = System.Drawing.Color.SteelBlue;
+            this.checkBox_audio.Checked = true;
+            this.checkBox_audio.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox_audio.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox_audio.Location = new System.Drawing.Point(702, 420);
+            this.checkBox_audio.Name = "checkBox_audio";
+            this.checkBox_audio.Size = new System.Drawing.Size(86, 18);
+            this.checkBox_audio.TabIndex = 7;
+            this.checkBox_audio.Text = "Sound on";
+            this.checkBox_audio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox_audio.UseVisualStyleBackColor = false;
+            this.checkBox_audio.CheckedChanged += new System.EventHandler(this.checkBox_audio_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Race.Properties.Resources.background_main;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox_audio);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.label_help);
+            this.Controls.Add(this.button_help);
+            this.Controls.Add(this.button_exit);
+            this.Controls.Add(this.button_settings);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form_main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form_main";
+            this.Load += new System.EventHandler(this.Form_main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_settings;
+        private System.Windows.Forms.Button button_exit;
+        private System.Windows.Forms.Button button_help;
+        private System.Windows.Forms.Label label_help;
+        private System.Windows.Forms.Button button_ok;
+        private System.Windows.Forms.CheckBox checkBox_audio;
+    }
+}
---
+++ Race/Form_settings.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Race
+{
+    partial class Form_settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_settings));
+            this.Box_color = new System.Windows.Forms.ComboBox();
+            this.button_apply = new System.Windows.Forms.Button();
+            this.picture_car = new System.Windows.Forms.PictureBox();
+            this.label_choose = new System.Windows.Forms.Label();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.Box_speed = new System.Windows.Forms.ComboBox();
+            this.label_speed = new System.Windows.Forms.Label();
+            this.picture_speed = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_car)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_speed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Box_color
+            // 
+            this.Box_color.BackColor = System.Drawing.Color.White;
+            this.Box_color.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Box_color.DropDownHeight = 105;
+            this.Box_color.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Box_color.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Box_color.FormattingEnabled = true;
+            this.Box_color.IntegralHeight = false;
+            this.Box_color.ItemHeight = 16;
+            this.Box_color.Items.AddRange(new object[] {
+            "red",
+            "blue",
+            "yellow",
+            "green",
+            "white"});
+            this.Box_color.Location = new System.Drawing.Point(125, 5);
+            this.Box_color.Name = "Box_color";
+            this.Box_color.Size = new System.Drawing.Size(75, 24);
+            this.Box_color.TabIndex = 4;
+            this.Box_color.SelectedIndexChanged += new System.EventHandler(this.Box_color_SelectedIndexChanged);
+            // 
+            // button_apply
+            // 
+            this.button_apply.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button_apply.Enabled = false;
+            this.button_apply.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_apply.Location = new System.Drawing.Point(527, 326);
+            this.button_apply.Name = "button_apply";
+            this.button_apply.Size = new System.Drawing.Size(75, 23);
+            this.button_apply.TabIndex = 5;
+            this.button_apply.Text = "Apply";
+            this.button_apply.UseVisualStyleBackColor = true;
+            // 
+            // picture_car
+            // 
+            this.picture_car.BackColor = System.Drawing.Color.Transparent;
+            this.picture_car.Location = new System.Drawing.Point(0, 40);
+            this.picture_car.Name = "picture_car";
+            this.picture_car.Size = new System.Drawing.Size(200, 320);
+            this.picture_car.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture_car.TabIndex = 6;
+            this.picture_car.TabStop = false;
+            // 
+            // label_choose
+            // 
+            this.label_choose.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.label_choose.AutoSize = true;
+            this.label_choose.BackColor = System.Drawing.Color.Transparent;
+            this.label_choose.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_choose.Location = new System.Drawing.Point(12, 9);
+            this.label_choose.Name = "label_choose";
+            this.label_choose.Size = new System.Drawing.Size(92, 16);
+            this.label_choose.TabIndex = 7;
+            this.label_choose.Text = "Choose a car";
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button_cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button_cancel.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_cancel.Location = new System.Drawing.Point(446, 326);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.TabIndex = 8;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // Box_speed
+            // 
+            this.Box_speed.BackColor = System.Drawing.Color.White;
+            this.Box_speed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Box_speed.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.Box_speed.FormattingEnabled = true;
+            this.Box_speed.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "4",
+            "5",
+            "10",
+            "20",
+            "23",
+            "46",
+            "92",
+            "115"});
+            this.Box_speed.Location = new System.Drawing.Point(481, 9);
+            this.Box_speed.Name = "Box_speed";
+            this.Box_speed.Size = new System.Drawing.Size(121, 21);
+            this.Box_speed.TabIndex = 9;
+            this.Box_speed.SelectedIndexChanged += new System.EventHandler(this.Box_speed_SelectedIndexChanged);
+            // 
+            // label_speed
+            // 
+            this.label_speed.AutoSize = true;
+            this.label_speed.BackColor = System.Drawing.Color.Transparent;
+            this.label_speed.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_speed.Location = new System.Drawing.Point(320, 10);
+            this.label_speed.Name = "label_speed";
+            this.label_speed.Size = new System.Drawing.Size(153, 16);
+            this.label_speed.TabIndex = 10;
+            this.label_speed.Text = "choose starting speed";
+            // 
+            // picture_speed
+            // 
+            this.picture_speed.BackColor = System.Drawing.Color.Transparent;
+            this.picture_speed.Location = new System.Drawing.Point(320, 40);
+            this.picture_speed.Name = "picture_speed";
+            this.picture_speed.Size = new System.Drawing.Size(280, 180);
+            this.picture_speed.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picture_speed.TabIndex = 11;
+            this.picture_speed.TabStop = false;
+            // 
+            // Form_settings
+            // 
+            this.AcceptButton = this.button_apply;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Race.Properties.Resources.background_settings;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.CancelButton = this.button_cancel;
+            this.ClientSize = new System.Drawing.Size(614, 361);
+            this.Controls.Add(this.picture_speed);
+            this.Controls.Add(this.label_speed);
+            this.Controls.Add(this.Box_speed);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.label_choose);
+            this.Controls.Add(this.picture_car);
+            this.Controls.Add(this.button_apply);
+            this.Controls.Add(this.Box_color);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form_settings";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Settings";
+            ((System.ComponentModel.ISupportInitialize)(this.picture_car)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_speed)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox Box_color;
+        private System.Windows.Forms.Button button_apply;
+        private System.Windows.Forms.PictureBox picture_car;
+        private System.Windows.Forms.Label label_choose;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.ComboBox Box_speed;
+        private System.Windows.Forms.Label label_speed;
+        private System.Windows.Forms.PictureBox picture_speed;
+    }
+}
